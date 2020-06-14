8400f8f758505f26c89c5db7ca696a425882a12d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-14 17:17:54 +0000

    Deploy Nikolas-Charalambidis/qr-spayd-invoice to github.com/Nikolas-Charalambidis/qr-spayd-invoice.git:gh-pages

---
+++ precache-manifest.616455fdafa2445f4a5e96191f7b159e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5669825766bd83eb167e987ee67ac8b8",
+    "url": "/spayd-invoice/index.html"
+  },
+  {
+    "revision": "89c238415e8f42cb76d5",
+    "url": "/spayd-invoice/static/css/main.df24737b.chunk.css"
+  },
+  {
+    "revision": "036d9af3b9ee3362bc85",
+    "url": "/spayd-invoice/static/js/2.bda70377.chunk.js"
+  },
+  {
+    "revision": "ef1c1dc921eb892faf80c713a1f15928",
+    "url": "/spayd-invoice/static/js/2.bda70377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c238415e8f42cb76d5",
+    "url": "/spayd-invoice/static/js/main.57cfde7e.chunk.js"
+  },
+  {
+    "revision": "980e3d46b5358636afc1",
+    "url": "/spayd-invoice/static/js/runtime-main.942d7abe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/qr-spayd-invoice/precache-manifest.1bbccb96b642133fd851b4398465b457.js"
+  "/spayd-invoice/precache-manifest.616455fdafa2445f4a5e96191f7b159e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/qr-spayd-invoice/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spayd-invoice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
